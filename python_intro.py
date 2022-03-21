ee1d4f35e34ed283e4a39a9cbbac40fba01ce350
sim <jasm0012@interia.pl>
2022-03-21 01:43:44 +0100

    My Django Girls app, first commit

---
+++ python_intro.py
@@ -0,0 +1,8 @@
+def hi(imie):
+    print('Witaj ' + imie + '!')
+
+dziewczyny = ['Rachel', 'Monica', 'Phoebe', 'Ola', 'Ty']
+for imie in dziewczyny:
+    hi(imie)
+    print('Kolejna dziewczyna')
+    .parent.parent
